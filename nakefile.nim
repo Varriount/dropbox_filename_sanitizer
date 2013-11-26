180af3963de229c3c46bab0b5761bc1cc3ff48b2
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-11-27 00:01:04 +0100

    Adds nakefile task to validate rst files.

--- nakefile.nim
+++ nakefile.nim
@@ -1,4 +1,4 @@
-import nake, os, times
+import nake, os, times, osproc
 
 let
   rst_files = @["LICENSE", "README", "docs/CHANGES",
@@ -21,18 +21,31 @@ proc needs_refresh(target: string, src: varargs[string]): bool =
     if srcTime > targetTime:
       return true
 
+iterator all_rst_files(): tuple[src, dest: string] =
+  for rst_name in rst_files:
+    var r: tuple[src, dest: string]
+    r.src = rst_name & ".rst"
+    # Ignore files if they don't exist, babel version misses some.
+    if not r.src.existsFile:
+      echo "Ignoring missing ", r.src
+      continue
+    r.dest = rst_name & ".html"
+    yield r
+
 task "doc", "Generates HTML docs":
   # Generate html files from the rst docs.
-  for rst_name in rst_files:
-    let rst_file = rst_name & ".rst"
-    # Ignore files if they don't exist, babel version misses some.
-    if not rst_file.existsFile:
-      echo "Ignoring missing ", rst_file
-      continue
-    let html_file = rst_name & ".html"
+  for rst_file, html_file in all_rst_files():
     if not html_file.needs_refresh(rst_file): continue
     if not shell("nimrod rst2html --verbosity:0", rst_file):
       quit("Could not generate html doc for " & rst_file)
     else:
-      echo "Generated " & rst_name & ".html"
+      echo rst_file & " -> " & html_file
   echo "All done"
+
+task "check_doc", "Validates rst format":
+  for rst_file, html_file in all_rst_files():
+    echo "Testing ", rst_file
+    let (output, exit) = execCmdEx("rst2html.py " & rst_file & " > /dev/null")
+    if output.len > 0 or exit != 0:
+      echo "Failed python processing of " & rst_file
+      echo output
